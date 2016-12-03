4f2d0447e4f1a2bdd5ccd3f1cdcf8792a3cee093
uxxxu <uxxxu@post.cz>
2016-12-03 14:42:33 +0100

    Update instrlist.h

--- instrlist.h
+++ instrlist.h
@@ -1,63 +1,63 @@
-#ifndef ILIST_H
-#define ILIST_H
+#ifndef INSTRLIST_H_
+#define INSTRLIST_H_
 
-/*
-	PROSTOR PRO SPOUSTU definu
-*/
-
-typedef struct 
-{
-	//typ instrukce
-	int TypInst;
-	// 1 adresa
-	void *adr1;
-	// 2 adresa
-	void *adr2;
-	//3 adresa
-	void *adr3;
-}tInst;
-
-typedef struct itemList
-{
-	tInst instr;
-	struct itemList *nItem;
-}tItemList;
+#define ADD_I		0
+#define SUB_I		1
+#define MUL_I		2
+#define DIV_I		3
+#define FADD_I		4
+#define FSUB_I		5
+#define FDIV_I		6
+#define VALPUSH_I	7
+#define TABPUSH_I	8
+#define CONV_I		9
+#define SWAP_I		10
+#define CONCAT_I	11
+#define LESS_I		12
+#define GREAT_I		13
+#define LESSEQ_I	14
+#define GREATEQ_I	15
+#define EQ_I		16
+#define NEQ_I		17
+#define FLESS_I		18
+#define FGREAT_I	19
+#define FLESSEQ_I	20
+#define FGREATEQ_I	21
+#define FEQ_I		22
+#define FNEQ_I		23
+#define GOTO_I		24
 
 typedef struct
 {
-	struct itemList *first;//prvni
-	struct itemList *last;//posledni
-	struct itemList *act;//aktivni
-}tInstList;
+  int instType;  // typ instrukce
+  void *addr1; // adresa 1
+  void *addr2; // adresa 2
+  void *addr3; // adresa 3
+} tInstr;
+
+typedef struct listItem
+{
+  tInstr instruction;
+  struct listItem *nextItem;
+} tListItem;
 
 
-//funkce inicializuici instrukcni seznam
-void InitL(tInstList *L);
-
-//dealokuje seznam 
-void FreeL(tInstList *L);
-
-//aktivace prvni instrukci
-void FirstL(tInstList *L);
-
-//nova instrukce na konci seznamu
-void ILastL(tInstList *L, tList I);
-
-//nasledujici = aktivni
-void NextL(tInstList *L);
-
-//nastavuje aktivni instrukci podle ukazatele
-void GoToL(tInstList *L, tItemList *GoTo);
-
-// vraci ukazatel na posledni instrukci
-void *GLPL(tInstList *L);
+typedef struct
+{
+  struct listItem *first;  // ukazatel na prvni prvek
+  struct listItem *last;   // ukazatel na posledni prvek
+  struct listItem *active; // ukazatel na aktivni prvek
+} tListOfInstr;
 
 
-tInst *getdata(tInstList *L);
+void listInit(tListOfInstr *L); // funkce inicializuje seznam instrukci
+void listFree(tListOfInstr *L); // funkce dealokuje seznam instrukci
+void listInsertLast(tListOfInstr *L, tInstr I); // vlozi novou instruci na konec seznamu
+void listFirst(tListOfInstr *L); // zaktivuje prvni instrukci
+void listNext(tListOfInstr *L); // aktivni instrukci se stane nasledujici instrukce
+tInstr *listGetData(tListOfInstr *L); // vrati aktivni instrukci
+void listGoto(tListOfInstr *L, tListItem *gotoInstr); // nastavime aktivni instrukci podle zadaneho ukazatele
+void *listGetPointerLast(tListOfInstr *L); // vrati ukazatel na posledni instrukci
 
-void i_add(int typ, void *p1, void *p2, void *p3);
-void i_print(tInst *inst);
-void il_print();
 
 #endif
-
