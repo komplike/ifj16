7f854c37f4f0fdef5229b0c16a9fb7feecfcc737
uxxxu <uxxxu@post.cz>
2016-12-03 14:48:51 +0100

    Update instrlist.c

--- instrlist.c
+++ instrlist.c
@@ -3,207 +3,122 @@
 #include "instrlist.h"
 #include "errors.h"
 
-tInstList *ilist;
+tListOfInstr *instr_list;
 
-void InitL(tInstList *L)
+void listInit(tListOfInstr *L)
 {
+
 	L->first = NULL;
-	L->act = NULL;
+	L->active = NULL;
 	L->last = NULL;
-}
-
-void FreeL(tInstList *L)
-{
-	for(L->act = L->first; L->first != NULL; L->first = L-> act)
-	{
-		L->act = L->first->nItem;
-		free(L->first);
-	}
-	L->last = NULL;
-}
-
-void FirstL(tInstList *L)
-{
-	L->act = L->first;
-}
-
-void ILastL(tInstList *L, tInst I)
-{
-	tItemList *item = malloc(sizeof(tItemList));
 	
+}
+
+void listFree(tListOfInstr *L){
+
+	for(L->active = L->first;L->first != NULL;L->first = L->active)
+		{
+			L->active = L->first->nextItem;		//zachovavame pointer na nastupny prvek
+			free(L->first);
+		}
+	L->last = NULL;
+
+}
+
+void listInsertLast(tListOfInstr *L, tInstr I){
+
+	tListItem * item;
+	item = malloc(sizeof(tListItem));
+
 	if(item == NULL)
 	{
-		ErrExit(99);	
+		printError(INT_ERR);
 	}
 
-	item->instr = I;
-	item->nItem = NULL;
+	item->instruction = I;
+	item->nextItem = NULL;
 
 	if(L->first != NULL)
 	{
-		L->last->nItem = item;
-		L->last = item;
-	}
+	    L->last->nextItem = item;
+	    L->last = item;
+	}    
 	else
-	{
-		L->first = l->Last = item;
-	}
-}
-
-void NextL(tInstList *L)
 {
-	if(L->active != NULL)
-	{
-		L->act = L->act->nItem;
+		L->first = L->last = item;
 	}
+
 }
 
-void GoToL(tInstList *L, tItemList *GoTo)
+void printInstruction(tInstr *I) 
 {
-	L->active = GoTo;
+    printf("%p ", (void *)I);
+
+    switch (I->instType) {
+        case ADD_I: printf("ADD"); break;
+        case SUB_I: printf("SUB"); break;
+        case MUL_I: printf("MUL"); break;
+        case DIV_I: printf("DIV"); break;
+        case FADD_I: printf("FADD"); break;
+        case FSUB_I: printf("FSUB"); break;
+        case FMUL_I: printf("FMUL"); break;
+        case FDIV_I:printf("FDIV"); break;
+        case VALPUSH_I: printf("VAL_PUSH"); break;
+        case TABPUSH_I: printf("TAB_PUSH"); break;
+        case CONV_I: printf("CONV"); break;
+        case SWAP_I: printf("SWAP"); break;
+        case CONCAT_I: printf("CONCAT"); break;
+        case LESS_I: printf("LESS"); break;
+        case GREAT_I: printf("GREAT"); break;
+        case LESSEQ_I: printf("LESSEQ"); break;
+        case GREATEQ_I: printf("GREATEQ"); break;
+        case EQ_I: printf("EQ"); break;
+        case NEQ_I: printf("NEQ"); break;
+        case FLESS_I: printf("FLESS"); break;
+        case FGREAT_I: printf("FGREAT"); break;
+        case FLESSEQ_I: printf("FLESSEQ"); break;
+        case FGREATEQ_I: printf("FGREATEQ"); break;
+        case FEQ_I: printf("FEQ"); break;
+        case FNEQ_I: printf("FNEQ"); break;
+        case GOTO_I: printf("GOTO"); break;
+        default: printf("%d", I->instType);
+    }
+
+    printf(" %p %p %p, ", I->addr1, I->addr2, I->addr3);
 }
 
-void *GLPL(tInstList *L)
+
+
+void listFirst(tListOfInstr *L){
+
+	 L->active = L->first;
+
+}
+
+void listNext(tListOfInstr *L){
+
+	if (L->active != NULL)
+  		L->active = L->active->nextItem;
+
+}
+
+tInstr *listGetData(tListOfInstr *L)
+{
+
+  if (L->active == NULL)
+  {
+    printError(INT_ERR);
+  }
+  else return &(L->active->instruction);
+
+}
+
+void listGoto(tListOfInstr *L, tListItem *gotoInstr)
+{
+  L->active =  gotoInstr;
+}
+
+void *listGetPointerLast(tListOfInstr *L)
 {
 	return (void *)L->last;
 }
-
-tInst *getdata(tInstList *L)
-{
-	if(L->act == NULL)
-	{
-		ErrExit(99);
-	}
-	else
-	{
-		return	&(L->act->instr);
-	}
-}
-
-void i_add(int typ, void *p1, void *p2, void *p3)
-{
-	tInst instrukce = { typ, p1, p2, p3};
-	
-	ILastL(ilist, instrukce);
-}
-
-void i_print(tInst *inst)
-{
-	switch(inst -> TypInst)
-	{
-//int
-		case ADD_I:
-			printf("ADD");
-			break;
-		case SUB_I:
-			printf("SUB");
-			break;
-		case MUL_I:
-			printf("MUL");
-			break;
-		case DIV_I:
-			printf("DIV");
-			break;
-		case LESS_I:
-			printf("LESS");
-			break;
-		case GREAT_I:
-			printf("GREAT");
-			break;
-		case LESEQ_I:
-			printf("LESEQ");
-			break;
-		case GREATEQ_I:
-			printf("GREATEQ");
-			break;
-		case EQ_I:
-			printf("EQ");
-			break;
-		case NEQ_I:
-			printf("NEQ");
-			break;
-//floats
-		case FADD_I:
-			printf("FADD");
-			break;
-		case FSUB_I:
-			printf("FSUB");
-			break;
-		case FMUL_I:
-			printf("FMUL");
-			break;
-		case FDIV_I:
-			printf("FDIV");
-			break;
-		case FLESS_I:
-			printf("FLESS");
-			break;
-		case FGREAT_I:
-			printf("FGREAT");
-			break;
-		case FLESEQ_I:
-			printf("FLESEQ");
-			break;
-		case FGREATEQ_I:
-			printf("FGREATEQ");
-			break;
-		case FEQ_I:
-			printf("FEQ");
-			break;
-		case FNEQ_I:
-			printf("FNEQ");
-			break;
-//------
-		case GOTO_I:
-			printf("GOTO");
-			break;
-		case RETURN_I:
-			printf("RETURN");
-			break;
-		case CALL_I:
-			printf("CALL");
-			break;
-		case VPUSH_I:
-			printf("VPUSH");
-			break;
-		case CONV_I:
-			printf("CONV");
-			break;
-		case SWAP_I:
-			printf("SWAP");
-			break;
-		case CONCAT_I:
-			printf("CONCAT");
-			break;
-		case TAB_I:
-			printf("TAB");
-			break;
-		case LABEL_I:
-			printf("LABEL");
-			break;
-		default:
-			printf("%d", inst->TypInst);
-	}
-	
-	printf("%p %p %p, " inst->adr1, inst->adr2, inst-> adr3);
-}
-
-void il_print()
-{
-	tInst *instruction;
-	
-	FirstL(ilist);
-	
-	while(ilist->active != NULL)
-	{
-		instruction = getdata(ilist);
-		
-		i_print(instruction);
-		printf("\n");
-		
-		NextL(ilist);
-	}
-}
-
-		
-
