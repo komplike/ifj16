984895a7dc63ab095ed20284c8bba99726812755
uxxxu <uxxxu@post.cz>
2016-12-07 16:57:51 +0100

    Update builtins.h

--- builtins.h
+++ builtins.h
@@ -1,8 +1,11 @@
-#ifndef BUILTINFUNCS
-#define BUILTINFUNCS
+#ifndef BUILTINS_H
+#define BUILTINS_H
 
 int lenght(char *text);
 char* substr(char *str, int i, int n);
 char* sort(char text[]);
 
+int readInt();
+double readDouble();
+char *readString();
 #endif
