d8c6c8f95c45fbcdbd2239f81b8f0472f6cdc844
uxxxu <uxxxu@post.cz>
2016-12-10 20:25:01 +0100

    Update ramce.h

--- ramce.h
+++ ramce.h
@@ -4,7 +4,7 @@
 
 typedef struct tRamec
 {	
-	tPoRamec *first;
+	tPoRamec *first;//prvni instrukce
 	tListItem *nextInstr;
 	struct tRamec *next;
 }tRamec;
@@ -16,16 +16,16 @@ typedef struct tPoRamec
 	struct tPoRamec *next;
 }tPoRamec;
 
-values get_val(htab_listitem *var);
-void set_val(htab_listitem *var, ival *val);
-void call_builtin_func(istack *stack, htab_listitem *built_in);
-int add_item(tRamec *ramec, htab_listitem *var);
-int set_frame(tRamec *ramec, htab_listitem *var, values val);
-int get_frame(tRamec *ramec, htab_listitem *var, values *val);
-int free_frame(tRamec **ramec);
+values get_val(htab_listitem *var);//ziska hodnotu
+void set_val(htab_listitem *var, ival *val);//nastavi hodnotu
+void call_builtin_func(istack *stack, htab_listitem *built_in);//zavolat vestavene funkce
+int add_item(tRamec *ramec, htab_listitem *var);//prida polozku
+int set_frame(tRamec *ramec, htab_listitem *var, values val);//nastavi ramec
+int get_frame(tRamec *ramec, htab_listitem *var, values *val);//ziska ramec
+int free_frame(tRamec **ramec);//uvolni ramec
 
-int return_i(tListofInstr *In);
-int call_i(tListOfInstr, istack *S, htab_listitem *f);
+int return_i(tListofInstr *In);//instrukce return
+int call_i(tListOfInstr, istack *S, htab_listitem *f);//volani funkce
 
 
 
